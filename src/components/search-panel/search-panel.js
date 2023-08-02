72d03066ce84b2af79ef8aaf14c6a80a9b324d2b
Lyutik13 <lyutikov.viktor.96@mail.ru>
2023-08-02 15:57:30 +0300

    endOne

--- src/components/search-panel/search-panel.js
+++ src/components/search-panel/search-panel.js
@@ -1,13 +1,31 @@
+import { Component } from 'react'
 import './search-panel.css'
 
-const SearchPanel = () => {
-	return (
-		<input
-			type="text"
-			className="form-control search-input"
-			placeholder="Найти сотрудника"
-		/>
-	)
+class SearchPanel extends Component {
+	constructor(props) {
+		super(props)
+		this.state = {
+			term: '',
+		}
+	}
+
+  onUpdateSearchThis = (e) => {
+    const term = e.target.value
+    this.setState({term})
+    this.props.onUpdateSearch(term)
+  }
+
+	render() {
+		return (
+			<input
+				type="text"
+				className="form-control search-input"
+				placeholder="Найти сотрудника"
+        value={this.state.term}
+        onChange={this.onUpdateSearchThis}
+			/>
+		)
+	}
 }
 
 export default SearchPanel
